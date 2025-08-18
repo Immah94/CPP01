c50df4285babe15ce96499eff69a9729186c735a
Imma Ranieri <immaranieri@Immas-MacBook-Air.local>
2025-08-18 15:45:36 +0200

    ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+int main(void)
+{
+    int N = 4;
+
+    Zombie *zombie = zombieHorde(N, "zombie");
+
+    for (int i = 0; i < N; i++)
+        zombie[i].announce();
+    
+    delete[] zombie;
+    return 0;
+}
