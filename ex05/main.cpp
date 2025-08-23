924ae89961fa4a9134e7d414f28af4c32dbbcdd3
Imma Ranieri <immaranieri@Immas-MacBook-Air.local>
2025-08-23 12:24:38 +0200

    Harl

---
+++ ex05/main.cpp
@@ -0,0 +1,18 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    std::string str;
+    Harl harl;
+
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+
+	return 0;
+}
